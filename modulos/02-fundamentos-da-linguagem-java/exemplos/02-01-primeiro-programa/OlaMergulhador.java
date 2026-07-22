25accf300b08827118f2017c40999b578a48c6eb
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2026-07-22 16:49:50 -0300

    feat(mod02): adiciona exemplo do primeiro programa java

---
+++ modulos/02-fundamentos-da-linguagem-java/exemplos/02-01-primeiro-programa/OlaMergulhador.java
@@ -0,0 +1,8 @@
+// asjbfjabsfa
+public class OlaMergulhador { // ou na frente de uma declaração ou instrução
+
+  public static void main(String[] args) {
+    // imprime a mensagem na saída padrão
+    //System.out.println("Olá, Mergulhador!");
+  }
+}
