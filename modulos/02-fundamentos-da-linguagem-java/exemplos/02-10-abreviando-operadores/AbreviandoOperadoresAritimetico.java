0b3df02c0f177b7dff28ffe6aa7ebba425a1deee
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2026-07-24 11:34:50 -0300

    feat(mod02): adiciona exemplo de abreviação de operadores

---
+++ modulos/02-fundamentos-da-linguagem-java/exemplos/02-10-abreviando-operadores/AbreviandoOperadoresAritimetico.java
@@ -0,0 +1,23 @@
+public class AbreviandoOperadoresAritimetico {
+
+  public static void main(String[] args) {
+    int quantidadeProdutos = 10;
+
+    // Isso aqui:
+    // quantidadeProdutos = quantidadeProdutos + 3;
+
+    // É o mesmo que isto:
+    // quantidadeProdutos += 3;
+
+    // quantidadeProdutos -= 3;
+
+    // quantidadeProdutos *= 3;
+
+    // quantidadeProdutos /= 3;
+
+    // quantidadeProdutos %= 3;
+
+
+    System.out.println(quantidadeProdutos);
+  }
+}
