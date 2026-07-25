cd29143c4cd1efe544487f7d2a945135d03ae9df
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2026-07-25 13:38:50 -0300

    feat(mod02): adiciona exemplos de tipos primitivos int e long

---
+++ modulos/02-fundamentos-da-linguagem-java/exemplos/02-13-int-long/ExemploInt.java
@@ -0,0 +1,11 @@
+public class ExemploInt {
+
+  public void main(String[] args) {
+    int populacaoUberlandia = 699_097;
+
+    int populacaoUberlandia2 = 2_147_483_647; // valor máximo
+
+    //int populacaoUberlandia3 = 2_147_483_648; // não compila
+
+  }
+}
---
+++ modulos/02-fundamentos-da-linguagem-java/exemplos/02-13-int-long/ExemploLong.java
@@ -0,0 +1,8 @@
+public class ExemploLong {
+
+  public void main(String[] args) {
+    long populacaoUberlandia = 699_097;
+
+    long populacaoUberlandia3 = 2_147_483_648L;
+  }
+}
