a23493a4644fa40a96e91aacd80d343551d18864
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2026-07-26 14:21:50 -0300

    feat(mod02): adiciona exemplos de promoção aritmética

---
+++ modulos/02-fundamentos-da-linguagem-java/exemplos/02-17-promocao-aritmetica/PromocaoAritimetica1.java
@@ -0,0 +1,10 @@
+public class PromocaoAritimetica1 {
+
+  public static void main(String[] args) {
+    int x = 10;
+    long y = 5;
+    long z = x * y;
+
+    System.out.println(z);
+  }
+}
---
+++ modulos/02-fundamentos-da-linguagem-java/exemplos/02-17-promocao-aritmetica/PromocaoAritimetica2.java
@@ -0,0 +1,10 @@
+public class PromocaoAritimetica2 {
+
+  public static void main(String[] args) {
+    long x = 10;
+    float y = 9.34f;
+    float z = x * y;
+
+    System.out.println(z);
+  }
+}
