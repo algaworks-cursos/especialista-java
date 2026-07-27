c502851e7e5fa7a37446b5cd1e13dd49ef64927e
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2026-07-27 18:18:50 -0300

    feat(mod02): adiciona exemplos usando sequências de escape

---
+++ modulos/02-fundamentos-da-linguagem-java/exemplos/02-20-sequencias-escape/SequenciasEscape.java
@@ -0,0 +1,12 @@
+public class SequenciasEscape {
+  
+  public static void main(String[] args) {
+    System.out.println("Oi \"Maria\"");
+
+    System.out.println("Seu nome:\nJoão");
+
+    System.out.println("C:\\Windows");
+
+
+  }
+}
