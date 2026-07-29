6e4047e58084bae86c4563298567d64d1d05db2b
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2026-07-29 11:30:50 -0300

    feat(mod03): adiciona exemplo de operadores de igualdade e negação

---
+++ modulos/03-estruturas-de-controle-e-operadores/exemplos/03-01-operadores-igualdade-negacao/OperadoresIgualdadeNegacao.java
@@ -0,0 +1,28 @@
+public class OperadoresIgualdadeNegacao {
+  public static void main(String[] args) {
+    // boolean numerosIguais = 10 == 10;
+    // boolean numerosIguais = (4 + 6) == (5 * 2);
+
+    int numero1 = 11;
+    int numero2 = 10; 
+    // boolean numerosIguais = numero1 == 10;
+    boolean numerosIguais = numero1 == numero2;
+
+    // boolean numerosDiferentes = !numerosIguais;
+    // boolean numerosDiferentes = !(numero1 == numero2);
+    boolean numerosDiferentes = numero1 != numero2;
+
+    System.out.printf("Números iguais: %b%n", numerosIguais);
+    // System.out.printf("Números iguais: %b%n", numero1 == numero2);
+
+    System.out.printf("Numeros diferentes %b%n", numerosDiferentes);
+
+
+    String nome1 = "Tiago";
+    String nome2 = "Tiago";
+
+    boolean nomesIguais = nome1 == nome2;
+
+    System.out.printf("Nomes iguais: %b%n", nomesIguais);
+  }
+}
