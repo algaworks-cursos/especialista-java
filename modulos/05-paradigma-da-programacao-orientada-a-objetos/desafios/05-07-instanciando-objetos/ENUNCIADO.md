ec4913348dc426d864dc82b1f5d34de3219fb89d
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2026-08-08 16:00:00 -0300

    feat(mod05): resolve desafio de instanciacao e acesso a propriedades

---
+++ modulos/05-paradigma-da-programacao-orientada-a-objetos/desafios/05-07-instanciando-objetos/ENUNCIADO.md
@@ -0,0 +1,11 @@
+# 05.07 — Desafio: Instanciando objetos e acessando os atributos
+
+Sua tarefa é criar uma classe que representa um `Cachorro`. 
+
+Um cachorro deve ter as seguintes propriedades (atributos):
+- Nome
+- Raça
+- Sexo
+- Idade
+
+Crie a classe de execução (`Principal`) com o método `main`. Nele, instancie dois objetos do tipo `Cachorro`, atribua valores para cada uma de suas propriedades e, por fim, imprima essas informações na tela (console).
---
+++ modulos/05-paradigma-da-programacao-orientada-a-objetos/desafios/05-07-instanciando-objetos/Principal.java
@@ -0,0 +1,32 @@
+public class Principal {
+    public static void main(String[] args) {
+        Cachorro cachorro1 = new Cachorro();
+        Cachorro cachorro2 = new Cachorro();
+
+        cachorro1.nome = "Bob";
+        cachorro1.raca = "Salsicha";
+        cachorro1.sexo = "Feminino";
+        cachorro1.idade = 18;
+
+        cachorro2.nome = "Princesa";
+        cachorro2.raca = "Pitbull";
+        cachorro2.sexo = "Masculino";
+        cachorro2.idade = 19;
+
+        System.out.println("Cachorro 1");
+        System.out.println("===========");
+        System.out.printf("Nome: %s%n", cachorro1.nome);
+        System.out.printf("Raça: %s%n", cachorro1.raca);
+        System.out.printf("Sexo: %s%n", cachorro1.sexo);
+        System.out.printf("Idade: %d%n", cachorro1.idade);
+
+        System.out.println();
+
+        System.out.println("Cachorro 2");
+        System.out.println("===========");
+        System.out.printf("Nome: %s%n", cachorro2.nome);
+        System.out.printf("Raça: %s%n", cachorro2.raca);
+        System.out.printf("Sexo: %s%n", cachorro2.sexo);
+        System.out.printf("Idade: %d%n", cachorro2.idade);
+    }
+}
