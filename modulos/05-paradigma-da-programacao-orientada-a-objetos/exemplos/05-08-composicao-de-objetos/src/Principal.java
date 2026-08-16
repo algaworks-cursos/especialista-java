1a91a6fb1243e64f1ad99b39593802444ade5ee8
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2026-08-16 09:30:00 -0300

    fix(mod05): corrige exemplo 05-08 removendo atribuicao antecipada de proprietario

--- modulos/05-paradigma-da-programacao-orientada-a-objetos/exemplos/05-08-composicao-de-objetos/src/Principal.java
+++ modulos/05-paradigma-da-programacao-orientada-a-objetos/exemplos/05-08-composicao-de-objetos/src/Principal.java
@@ -1,35 +1,29 @@
 public class Principal {
+
     public static void main(String[] args) {
         Carro meuCarro = new Carro();
         meuCarro.anoFabricacao = 2021;
-        meuCarro.cor = "Branco";
-        meuCarro.modelo = "Ford Ka";
-        meuCarro.fabricante = "Ford";
-
-        Pessoa dono1 = new Pessoa();
-        dono1.nome = "Gustavo";
-        dono1.cpf = "111.222.333-44";
-        dono1.anoNascimento = 1995;
-
-        meuCarro.proprietario = dono1;
+        meuCarro.cor = "Preto";
+        meuCarro.fabricante = "Honda";
+        meuCarro.modelo = "HR-V";
 
         Carro seuCarro = new Carro();
-        seuCarro.anoFabricacao = 2026;
-        seuCarro.cor = "Preto";
-        seuCarro.modelo = "Fusion";
-        seuCarro.fabricante = "Ford";
+        seuCarro.anoFabricacao = 2022;
+        seuCarro.cor = "Branco";
+        seuCarro.fabricante = "BMW";
+        seuCarro.modelo = "X6";
 
         System.out.println("Meu carro");
-        System.out.println("----------");
-        System.out.printf("Modelo do meu carro: %s%n", meuCarro.modelo);
+        System.out.println("---------");
+        System.out.printf("Modelo: %s%n", meuCarro.modelo);
         System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
-        System.out.printf("Proprietário: %s%n", meuCarro.proprietario.nome);
 
         System.out.println();
 
         System.out.println("Seu carro");
-        System.out.println("----------");
-        System.out.printf("Modelo do seu carro: %s%n", seuCarro.modelo);
+        System.out.println("---------");
+        System.out.printf("Modelo: %s%n", seuCarro.modelo);
         System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
     }
+
 }
